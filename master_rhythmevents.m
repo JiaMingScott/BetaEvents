ed6d5cd63ed7c4c1bd9c7a251f91a53b58d70902
Jia-Ming Scott <jm800268@dal.ca>
2019-01-10 14:55:25 -0400

    commenting out mouse analysis

--- master_rhythmevents.m
+++ master_rhythmevents.m
@@ -5,19 +5,19 @@ rhythmid='beta';
 
 % dataset type : 1 for mouse detection, 2 for human detection, 3 for human attention
 %% mouse preprocess TFR
-datatypeid='mousedetection';
-subject_list={'mouse1_session1', 'mouse1_session2', 'mouse1_session3', 'mouse1_session4', 'mouse1_session5', ....
-  'mouse2_session1', 'mouse2_session2', 'mouse2_session3', 'mouse2_session4', 'mouse2_session5'};
-
-for s=1:10
-  clearvars -except subject_list subjind thrFOM rhythm_band_inds rhythmid  
-  
-  load(strcat(resultPath, '\prestim_', datatypeid, '_', subject_list{s}, '.mat'))
-    
-  indsoi = 1:length(tVec); % Indices correspond to t = -1000:0 ms.
-  rhythm_localmax_analysis(resultPath, datatypeid, subject_list, s, rhythmid, rhythm_band_inds, tVec, fVec, indsoi, prestim_TFR_yes_no, YorN)
-  rhythm_event_analysis(rhythmid, datatypeid, resultPath, subject_list, s, thrFOM, tVec, fVec, prestim_TFR_yes_no, YorN)
-end
+% datatypeid='mousedetection';
+% subject_list={'mouse1_session1', 'mouse1_session2', 'mouse1_session3', 'mouse1_session4', 'mouse1_session5', ....
+%   'mouse2_session1', 'mouse2_session2', 'mouse2_session3', 'mouse2_session4', 'mouse2_session5'};
+% 
+% for s=1:10
+%   clearvars -except subject_list subjind thrFOM rhythm_band_inds rhythmid  
+%   
+%   load(strcat(resultPath, '\prestim_', datatypeid, '_', subject_list{s}, '.mat'))
+%     
+%   indsoi = 1:length(tVec); % Indices correspond to t = -1000:0 ms.
+%   rhythm_localmax_analysis(resultPath, datatypeid, subject_list, s, rhythmid, rhythm_band_inds, tVec, fVec, indsoi, prestim_TFR_yes_no, YorN)
+%   rhythm_event_analysis(rhythmid, datatypeid, resultPath, subject_list, s, thrFOM, tVec, fVec, prestim_TFR_yes_no, YorN)
+% end
 
 %% human detection data
 datatypeid='humandetection';
